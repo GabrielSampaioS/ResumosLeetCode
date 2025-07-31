4e57e3e9962d55ec4ddcdbe0e0cb6ed5a1de2a84
Gabriel <gabrielssampaio42@gmail.com>
2025-07-31 18:17:20 -0300

    add 1353

---
+++ 1353. Número máximo de eventos que podem ser atendidos/ex1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> // for std::sort
+using namespace std;
+
+
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        sort(events.begin(), events.end());
+
+
+    return 1;
+        
+    }
+};
+
+int main() {
+
+    
+    /*sort(events.begin(), events.end());
+    for(auto& n : events){
+        cout << n[0] << " " << n[1] << endl;
+    }*/
+    
+    //CMD não fechar
+    int x; cin >> x; 
+    return 0;
+}
